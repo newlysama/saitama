91e7810462fedbcb701475c2baf6d8e728a8b5ee
Thibault Thomas <thibault.thomas71@gmail.com>
2025-03-29 11:26:45 +0100

    add: pretty linked list comments

--- utils/linked_lists/linked_lists.cpp
+++ utils/linked_lists/linked_lists.cpp
@@ -132,6 +132,7 @@ std::tuple<std::unique_ptr<LinkedList>, std::unique_ptr<LinkedList>> LinkedList:
 
     size_t split_index = list->size / 2;
 
+    // Don't need to modify emplace, since we use unique ptrs anyway
     std::unique_ptr<LinkedList> new_list1 = std::make_unique<LinkedList>();
     std::unique_ptr<LinkedList> new_list2 = std::make_unique<LinkedList>();
 
