c91084881c9111a3471659a59e46c42c7151e600
Thibault Thomas <thibault.thomas71@gmail.com>
2025-04-03 12:30:46 +0200

    added some more specific test selectors

--- tests/quicksort_mergesort/test_selector.hh
+++ tests/quicksort_mergesort/test_selector.hh
@@ -5,8 +5,10 @@
  *        Simply comment/uncomment the maccros 
  */
 
-// #define QUICK_SORT_LINKED_LISTS
-// #define QUICK_SORT_VECTORS
+// #define QUICK_SORT_LOMUTO_LINKED_LISTS
+#define QUICK_SORT_HOARE_LINKED_LISTS
+// #define QUICK_SORT_LOMUTO_VECTORS
+// #define QUICK_SORT_HOARE_VECTORS
 
-#define MERGE_SORT_LINKED_LISTS
+// #define MERGE_SORT_LINKED_LISTS
 // #define MERGE_SORT_VECTORS
