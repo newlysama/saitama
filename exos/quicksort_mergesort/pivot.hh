19d1bd042eb936518e4fabec5d9019ac6a1b5884
Thibault Thomas <thibault.thomas71@gmail.com>
2025-04-08 16:21:46 +0200

    fix: trailing whitespaces

--- exos/quicksort_mergesort/pivot.hh
+++ exos/quicksort_mergesort/pivot.hh
@@ -2,14 +2,14 @@
 
 /**
  * @brief Chose which pivot strategy to use for quicksort
- * 
+ *
  */
 
  #define RANDOM 1
  #define FIRST  2
  #define LAST   3
  #define MID    4
- 
+
 #ifndef PIVOT
 #define PIVOT RANDOM
 #endif
