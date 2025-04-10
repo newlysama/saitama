ffb570ebb3b8c968473542277ad202654ca7eeb2
Thibault Thomas <thibault.thomas71@gmail.com>
2025-04-10 17:08:37 +0200

    add: oneTBB lib locally

--- external/lib/cmake/TBB/TBBTargets-relwithdebinfo.cmake
+++ external/lib/cmake/TBB/TBBTargets-relwithdebinfo.cmake
@@ -8,33 +8,33 @@ set(CMAKE_IMPORT_FILE_VERSION 1)
 # Import target "TBB::tbb" for configuration "RelWithDebInfo"
 set_property(TARGET TBB::tbb APPEND PROPERTY IMPORTED_CONFIGURATIONS RELWITHDEBINFO)
 set_target_properties(TBB::tbb PROPERTIES
-  IMPORTED_LOCATION_RELWITHDEBINFO "${_IMPORT_PREFIX}/lib/libtbb.so.12.16"
+  IMPORTED_LOCATION_RELWITHDEBINFO "${_IMPORT_PREFIX}/lib/libtbb.so.12.15"
   IMPORTED_SONAME_RELWITHDEBINFO "libtbb.so.12"
   )
 
 list(APPEND _cmake_import_check_targets TBB::tbb )
-list(APPEND _cmake_import_check_files_for_TBB::tbb "${_IMPORT_PREFIX}/lib/libtbb.so.12.16" )
+list(APPEND _cmake_import_check_files_for_TBB::tbb "${_IMPORT_PREFIX}/lib/libtbb.so.12.15" )
 
 # Import target "TBB::tbbmalloc" for configuration "RelWithDebInfo"
 set_property(TARGET TBB::tbbmalloc APPEND PROPERTY IMPORTED_CONFIGURATIONS RELWITHDEBINFO)
 set_target_properties(TBB::tbbmalloc PROPERTIES
-  IMPORTED_LOCATION_RELWITHDEBINFO "${_IMPORT_PREFIX}/lib/libtbbmalloc.so.2.16"
+  IMPORTED_LOCATION_RELWITHDEBINFO "${_IMPORT_PREFIX}/lib/libtbbmalloc.so.2.15"
   IMPORTED_SONAME_RELWITHDEBINFO "libtbbmalloc.so.2"
   )
 
 list(APPEND _cmake_import_check_targets TBB::tbbmalloc )
-list(APPEND _cmake_import_check_files_for_TBB::tbbmalloc "${_IMPORT_PREFIX}/lib/libtbbmalloc.so.2.16" )
+list(APPEND _cmake_import_check_files_for_TBB::tbbmalloc "${_IMPORT_PREFIX}/lib/libtbbmalloc.so.2.15" )
 
 # Import target "TBB::tbbmalloc_proxy" for configuration "RelWithDebInfo"
 set_property(TARGET TBB::tbbmalloc_proxy APPEND PROPERTY IMPORTED_CONFIGURATIONS RELWITHDEBINFO)
 set_target_properties(TBB::tbbmalloc_proxy PROPERTIES
   IMPORTED_LINK_DEPENDENT_LIBRARIES_RELWITHDEBINFO "TBB::tbbmalloc"
-  IMPORTED_LOCATION_RELWITHDEBINFO "${_IMPORT_PREFIX}/lib/libtbbmalloc_proxy.so.2.16"
+  IMPORTED_LOCATION_RELWITHDEBINFO "${_IMPORT_PREFIX}/lib/libtbbmalloc_proxy.so.2.15"
   IMPORTED_SONAME_RELWITHDEBINFO "libtbbmalloc_proxy.so.2"
   )
 
 list(APPEND _cmake_import_check_targets TBB::tbbmalloc_proxy )
-list(APPEND _cmake_import_check_files_for_TBB::tbbmalloc_proxy "${_IMPORT_PREFIX}/lib/libtbbmalloc_proxy.so.2.16" )
+list(APPEND _cmake_import_check_files_for_TBB::tbbmalloc_proxy "${_IMPORT_PREFIX}/lib/libtbbmalloc_proxy.so.2.15" )
 
 # Commands beyond this point should not need to know the version.
 set(CMAKE_IMPORT_FILE_VERSION)
